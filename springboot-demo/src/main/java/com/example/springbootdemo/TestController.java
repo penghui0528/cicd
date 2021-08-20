7c394fd9bff25d04c566cede98b146686a910d8b
18819305949@163.com <18819305949@163.com>
2021-08-20 18:27:21 +0800

    第一次提交

---
+++ springboot-demo/src/main/java/com/example/springbootdemo/TestController.java
@@ -0,0 +1,16 @@
+package com.example.springbootdemo;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String test() {
+        return "hello springboot k8s -------测试-- @@@$$$$";
+    }
+}
